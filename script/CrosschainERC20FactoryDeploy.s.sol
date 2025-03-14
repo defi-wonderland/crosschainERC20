a401552eb228cd503a9348a9b3946c4064b61b75
0xchin <alanracciatti1220@gmail.com>
2025-03-14 16:34:44 -0300

    chore: appropriate salt string

--- script/CrosschainERC20FactoryDeploy.s.sol
+++ script/CrosschainERC20FactoryDeploy.s.sol
@@ -13,7 +13,7 @@ import {CREATE3} from 'solady/utils/CREATE3.sol';
 contract DeployCrosschainERC20Factory is Script {
   function run() public returns (CrosschainERC20Factory _factory) {
     vm.startBroadcast();
-    bytes32 salt = keccak256(abi.encodePacked('WONDERLAND LOVES PENGUINS')); // Deploys at `0x0b1772D3f03f4f21Faf2Ca5aa8689e6d13337aF3`
+    bytes32 salt = keccak256(abi.encodePacked('wonderland')); // Deploys at `0x0b1772D3f03f4f21Faf2Ca5aa8689e6d13337aF3`
     _factory = CrosschainERC20Factory(CREATE3.deployDeterministic(type(CrosschainERC20Factory).creationCode, salt));
     vm.stopBroadcast();
   }

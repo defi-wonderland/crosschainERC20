5b4f4e439fa3b44622085ace1b6adb6ef418f4f7
0xNick <3832465+0xNikolas@users.noreply.github.com>
2025-04-30 17:52:32 +0300

    refactor: remove duplicate interface functions

--- src/interfaces/ICrosschainERC20.sol
+++ src/interfaces/ICrosschainERC20.sol
@@ -7,14 +7,4 @@ import {IERC7802} from 'interfaces/external/IERC7802.sol';
 
 /// @title ICrosschainERC20
 /// @notice This interface is available on the CrosschainERC20 contract.
-interface ICrosschainERC20 is IXERC20, IERC7802 {
-  /// @notice Allows a bridge to mint tokens.
-  /// @param _to     Address to mint tokens to.
-  /// @param _amount Amount of tokens to mint.
-  function crosschainMint(address _to, uint256 _amount) external;
-
-  /// @notice Allows a bridge to burn tokens.
-  /// @param _from   Address to burn tokens from.
-  /// @param _amount Amount of tokens to burn.
-  function crosschainBurn(address _from, uint256 _amount) external;
-}
+interface ICrosschainERC20 is IXERC20, IERC7802 {}

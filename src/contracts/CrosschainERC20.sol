a5fcb1af477e372a6954da6e8d90af237e2d7269
0xchin <alanracciatti1220@gmail.com>
2025-05-23 09:36:42 -0300

    chore: fix linter error

--- src/contracts/CrosschainERC20.sol
+++ src/contracts/CrosschainERC20.sol
@@ -28,18 +28,6 @@ contract CrosschainERC20 is XERC20, ICrosschainERC20 {
     address __factory
   ) XERC20(__name, __symbol, __decimals, __factory) {}
 
-  /// @inheritdoc ERC20
-  function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-    // If minting, check if the receiver is valid
-    if (from == address(0)) {
-      if (to == address(0) || to == address(this)) {
-        revert CrosschainERC20__InvalidReceiver(to);
-      }
-    }
-
-    super._beforeTokenTransfer(from, to, amount);
-  }
-
   /// @inheritdoc IERC7802
   function crosschainMint(address _to, uint256 _amount) external {
     _mintWithCaller(msg.sender, _to, _amount);
@@ -62,5 +50,17 @@ contract CrosschainERC20 is XERC20, ICrosschainERC20 {
   function supportsInterface(bytes4 _interfaceId) public view virtual returns (bool) {
     return _interfaceId == type(IERC7802).interfaceId || _interfaceId == type(IERC20).interfaceId
       || _interfaceId == type(IERC165).interfaceId || _interfaceId == type(IXERC20).interfaceId;
+  }
+
+  /// @inheritdoc ERC20
+  function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+    // If minting, check if the receiver is valid
+    if (from == address(0)) {
+      if (to == address(0) || to == address(this)) {
+        revert CrosschainERC20__InvalidReceiver(to);
+      }
+    }
+
+    super._beforeTokenTransfer(from, to, amount);
   }
 }
